e1d13c54de628f1c6154c9f4b7c7f0b6f06eb9c5
grahamfyfecodes <67826423+grahamfyfecodes@users.noreply.github.com>
2021-12-08 12:28:34 +0000

    Init commit

---
+++ src/main/java/com/fyfe/cairds/CairdsApplication.java
@@ -0,0 +1,13 @@
+package com.fyfe.cairds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CairdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CairdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fyfe/cairds/CairdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyfe.cairds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CairdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
